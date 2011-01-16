56e534fe9a1e44029172103bb19e2da130756d5e
Jason Mansfield <trentonknight@gmail.com>
2011-01-15 19:04:41 -0500

    downgraded

--- lettersLL-ASSN1.cpp
+++ lettersLL-ASSN1.cpp
@@ -11,10 +11,6 @@ struct NODES{
 };
 
 void openFILE(ifstream& file);
-bool antiNUMBERS(string wordIN);
-string characterCHECK(string wordIN);
-string formatWORDS(string wordIN);
-
 
 int main(){
   string word;
@@ -25,19 +21,16 @@ int main(){
   arrayOfPointers[26] = new NODES;
   openFILE(grabFILE);
   if(grabFILE.is_open()){
-    while(grabFILE){
-      grabFILE >> word;
-      removeNUM = antiNUMBERS(word);
-      if(removeNUM){
-      word = characterCHECK(word);
-      word =  formatWORDS(word);
-      cout << "Approved word: " << word << endl;
-      }
+    while(!grabFILE.eof()){
+     grabFILE >> word;
+     cout << "word:" << word << endl;
+     word.clear();
     }
-    
-  }
-  
+    grabFILE.close();
+    }
+
 }
+
 void openFILE(ifstream& file){
   string filename;
 
@@ -49,53 +42,4 @@ void openFILE(ifstream& file){
     openFILE(file);
   }
 }
-bool antiNUMBERS(string wordIN){
- int count = 0;
- bool noNUMBERS = true;
- int wordLENGTH = wordIN.length();///get length of individual words
-  while(wordLENGTH != 0){///set for end of word halt
-    if(isdigit(wordIN[count])){
-      noNUMBERS = false;
-    }
-  count++;///increment to count through each character in the array
-  --wordLENGTH;///halts when end of word is reached
-  } 
-  return noNUMBERS;
-}
-string characterCHECK(string wordIN){
-  int count = 0, index = 0;
- int wordLENGTH = wordIN.length();///get length of individual words
- string *pt = 0;
- char wordOUT[wordLENGTH];
-  
-  while(wordLENGTH != 0){///set for end of word halt
-    if(isalpha(wordIN[count])){
-      wordOUT[index] = wordIN[count];
-      index++;
-    }
-    else{
-      pt = new string[1];
-      *pt = wordIN[count];
-      wordIN[count] = wordIN[count + 1];
-      delete [] pt;
-    }
-  count++;///increment to count through each character in the array
-  --wordLENGTH;///halts when end of word is reached
-  } 
-  wordIN((LPCTSTR)wordOUT);
-  wordIN(wordLENGTH, index)
-  return wordIN;
-}
-string formatWORDS(string wordIN){
-  int count = 0;
-  int wordLENGTH = wordIN.length();///get length of individual words
-  while(wordLENGTH != 0){///set for end of word halt
-  wordIN[count] = toupper(wordIN[count]);///set each character in array to uppercase
-  count++;///increment to count through each character in the array
-  --wordLENGTH;///halts when end of word is reached
-  }
-  return wordIN;
-}
-
-
 
